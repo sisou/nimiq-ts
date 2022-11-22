2b293594e571e4505e3a229cfe35db4ae0253577
Sören <hello@soerenschwert.de>
2022-11-22 10:43:46 +0100

    Remove unnecessary classes from the exported API

--- src/lib.ts
+++ src/lib.ts
@@ -1,17 +1,17 @@
-// Root classes without imports
-export { ArrayUtils } from "./ArrayUtils";
-export { CRC8 } from "./CRC8";
+/* Root classes without imports */
+// export { ArrayUtils } from "./ArrayUtils";
+// export { CRC8 } from "./CRC8";
 export { GenesisConfig } from "./GenesisConfig";
-export { NumberUtils } from "./NumberUtils";
-export { StringUtils } from "./StringUtils";
-export { WasmHelper } from "./WasmHelper";
+// export { NumberUtils } from "./NumberUtils";
+// export { StringUtils } from "./StringUtils";
+// export { WasmHelper } from "./WasmHelper";
 
-// Buffers
+/* Buffers */
 export { BufferUtils } from "./BufferUtils";
 export { SerialBuffer } from "./SerialBuffer";
 
-// Base classes
-export { Serializable } from "./Serializable";
+/* Base classes */
+// export { Serializable } from "./Serializable";
 
 export { Address } from "./Address";
 export { CryptoUtils } from "./CryptoUtils";
@@ -22,14 +22,14 @@ export { KeyPair } from "./KeyPair";
 export { MerklePath } from "./MerklePath";
 export { MerkleTree } from "./MerkleTree";
 export { MnemonicUtils } from "./MnemonicUtils";
-export { PeerId } from "./PeerId";
+// export { PeerId } from "./PeerId";
 export { PrivateKey } from "./PrivateKey";
 export { PublicKey } from "./PublicKey";
 export { Secret } from "./Secret";
 export { Signature } from "./Signature";
 export { SignatureProof } from "./SignatureProof";
 
-// Must be defined at the end to not cause ordering problems
+/* Must be defined at the end to not cause ordering problems */
 export { Account } from "./Account"; // Needed for its namespace
 export { BasicAccount } from "./BasicAccount";
 
